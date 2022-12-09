ee88d9aba40f24ee0cf72422f3c2d3956028bbd7
SourPineapples <balogunostella@gmail.com>
2022-12-09 13:33:43 -0800

    insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+		return (0);
+}
