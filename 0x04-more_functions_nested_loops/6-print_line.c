b863db1d152e42a95c31398f638cc8989e2de370
SourPineapples <balogunostella@gmail.com>
2022-12-16 11:54:46 -0800

    seventh

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line in the termina
+ * @n: int in use
+ */
+void print_line(int n)
+{
+	int y;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (y = 0; y <= n; y++)
+		_putchar('_');
+	_putchar('\n');
+}
