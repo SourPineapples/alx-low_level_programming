23ed7bd233ed703e1fe00c5141bf41a9853f10df
SourPineapples <balogunostella@gmail.com>
2022-12-16 12:26:17 -0800

    eight

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include"main.h"
+/**
+ * print_diagonal -  draws a diagonal line on the terminal
+ * @a: int
+ */
+void print_diagonal(int n)
+{
+	int z;
+
+	if (a <= 0)
+		_putchar('\n');
+	else
+	{
+		for (z = 0; z < a; z++)
+		{
+			for (i = 0; i <= x; i++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
