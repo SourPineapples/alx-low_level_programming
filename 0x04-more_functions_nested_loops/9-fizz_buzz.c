0be2af7c2df0229d9bd39f3a6c9b5a156b2f2079
SourPineapples <balogunostella@gmail.com>
2022-12-16 13:32:43 -0800

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,13 +8,13 @@ void fizz_buzz(void)
 
 	for (w = 1; w < 100; w++)
 	{
-		if (w == 100)
+		if (w % 5 == 0)
 			printf("Buzz ");
 		else if (w % 3 == 0 && w % 5 == 0)
 			printf("FizzBuzz ");
 		else if (w % 3 == 0)
 			printf("Fizz ");
-		else if (w % 5 == 0)
+		else if (w == 3 * 5)
 			printf("%d ", w);
 	}
 	printf('\n');
