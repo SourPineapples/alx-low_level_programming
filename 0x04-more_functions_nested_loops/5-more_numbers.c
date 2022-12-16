f74f4fd7487d53420f3f62cf16a05c4188d17677
SourPineapples <balogunostella@gmail.com>
2022-12-16 11:44:33 -0800

    sixth

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * more_numbers - function that prints 10 times the numbers
+ */
+void more_numbers(void)
+{
+	int b, c;
+
+	for (b = 0; b < 10; b++)
+	{
+		for (c = 0; c < 15; c++)
+		{
+			_putchar(c + '0');
+		}
+		_putchar('\n');
+	}
+}
