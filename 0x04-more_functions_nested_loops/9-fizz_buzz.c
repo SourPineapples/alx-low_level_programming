78b1140068e66709633b429f226c5dc68f53ff74
SourPineapples <balogunostella@gmail.com>
2022-12-16 13:24:16 -0800

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * fizz_buzz - designed to help filter out programming job candidates
+ */
+void fizz_buzz(void)
+{
+	int w;
+
+	for (w = 1; w < 100; w++)
+	{
+		if (w == 100)
+			printf("Buzz ");
+		else if (w % 3 == 0 && w % 5 == 0)
+			printf("FizzBuzz ");
+		else if (w % 3 == 0)
+			printf("Fizz ");
+		else if (w % 5 == 0)
+			printh("%d ", x);
+	}
+	printf('\n');
+}
