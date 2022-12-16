fe268e9e5d0650ae11e258a91d30bf0c1e7090da
SourPineapples <balogunostella@gmail.com>
2022-12-16 11:21:32 -0800

    fourth

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints the numbers, from 0 to 9
+ * @n: int in question
+ * Return: success
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		_putchar(n + '0');
+	_putchar('\n');
+}
