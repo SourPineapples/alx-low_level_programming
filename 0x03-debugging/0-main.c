3f304cab8a97f84a2aced5081dc18a1b9257df06
SourPineapples <balogunostella@gmail.com>
2022-12-16 08:56:06 -0800

    debug1

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,28 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <main.h>
 /**
- * positive_or_negative - debugging tests function that prints integer
- * @i: int in use
+ * main - debugging tests function that prints integer
  * Return: 0
  */
-
-void positive_or_negative(int i)
+int main(void)
 {
-	int n;
-	int i;
+	int a;
 
 	i = 0;
+	positive_or_negative(i);
 
-	srand(time(0));
-	n = i;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	if (n == 0)
-		printf("%d is zero\n", n);
-	if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
 	return (0);
 }
