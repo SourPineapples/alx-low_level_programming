01e6219805694dbd9a3c41e98f7c9267df9c0cb4
SourPineapples <balogunostella@gmail.com>
2022-12-19 10:28:45 -0800

    task5

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - function that prints a string, in reverse
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int i, n;
+
+	s = "hello"
+	n = strlen(s);
+
+	for (i = n - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
