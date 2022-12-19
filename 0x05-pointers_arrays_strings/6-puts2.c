dc5ea086bdd4968b9d584165d502dcbbf45e9f74
SourPineapples <balogunostella@gmail.com>
2022-12-19 11:28:50 -0800

    task7

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts2 - function that prints every other character of a string
+ * @str: function that prints
+ */
+void puts2(char *str)
+{
+	int len;
+
+	len = strlen(str);
+	for (i = 0; i < len; i++)
+		_putchar(str[i]);
+	_puthar('\n');
+}
