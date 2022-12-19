a77363093ca0733284d0b69498ed1736f808c3a2
SourPineapples <balogunostella@gmail.com>
2022-12-19 09:58:36 -0800

    task3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strlen -  function that returns the length of a string
+ * @s: string pointer
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
