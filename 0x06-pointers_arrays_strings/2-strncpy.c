33d084606adc1c3bfc2bd74cb75facafbf596a26
SourPineapples <balogunostella@gmail.com>
2022-12-23 02:53:55 -0800

    2-strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncpy -  function that copies a string
+ * @dest: first char
+ * @src: second char
+ * @n: int in use
+ * Return: string dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	_strncpy(dest, src, n);
+	return (dest);
+}
