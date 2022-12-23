34a2a4a6f68f2bd43b0e5b59bf51dc981132e15a
SourPineapples <balogunostella@gmail.com>
2022-12-23 05:18:56 -0800

    cap_string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+/**
+ * cap_string -  capitalizes all words of a string
+ * @v: char in use
+ * Return: string
+ */
+char *cap_string(char *v)
+{
+	char spc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	int len = 13;
+	int a = 0, i;
+
+	while (v[a])
+	{
+		i = 0;
+		while (i < len)
+		{
+			if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
+				s[a] = s[a] - 32;
+			i++;
+		}
+		a++;
+	}
+	return (v);
+}
