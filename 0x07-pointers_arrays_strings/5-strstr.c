f54b6ae1f1dc96584d5e7dc8f844c41db65a89b8
SourPineapples <balogunostella@gmail.com>
2023-01-02 03:46:58 -0800

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strstr -locates a substring
+ * @haystack: big string
+ * @needle: substring
+ * Return: beginning of needle or NULL
+ */
+char *_strstr(char *haystack, char *needle);
+{
+	int a = 0, z = 0;
+
+	while (haystack[a])
+	{
+		while (needle[z])
+		{
+			if (haystack[a + z] != needle[z])
+		{
+			break;
+		}
+		z++;
+	}
+
+	if (needle[z] == '\0')
+	{
+		return (haystack + a);
+	}
+	a++;
+	}
+	return ('\0');
+}
