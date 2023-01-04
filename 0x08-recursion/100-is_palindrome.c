5c4fa26303a5c126de426ca96fdacfb183a9f3e6
SourPineapples <balogunostella@gmail.com>
2023-01-04 07:07:58 -0800

    recursion100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -11,6 +11,45 @@ int is_palindrome(char *s);
  */
 int find_strlen(char *s)
 {
-	int len = 0;
-	if (*(s + len))
+	if (*s == '\0')
+		return (0);
+	return (1 + find_strlen(s + 1));
+}
+
+/**
+ * check_palindrome - checks for palindrome
+ * @s: string
+ * @len: length
+ * @i: int
+ * Return: 1 or 0
+ */
+int check_palindrome(char *s, int len, int i)
+{
+	if (len > 0)
 	{
+		if (s[i] == s[len])
+		{
+			return (check_palindrome(i + 1, len - 1, s));
+		}
+		else if (s[i] != s[len])
+		{
+			return (0);
+		}
+		else
+		{
+			return (1);
+		}
+	}
+	return (1);
+}
+
+
+/**
+ * is_palindrome - shows palindrome
+ * @s: string
+ * Return: 1 or 0
+ */
+int is_palindrome(char *s)
+{
+	return (check_palindrome(0, find_strlen(s) - 1, s));
+}
