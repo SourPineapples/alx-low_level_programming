f6190bbd7a8a59c9eff8d9f73a692e4e92920354
SourPineapples <balogunostella@gmail.com>
2023-01-10 10:08:18 -0800

    malloc1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,68 @@
+#include "main.h"
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: string
+ * Return: NULL or new string
+ */
+char *_strdup(char *str)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * _strcpy - copies arrays
+ * @src: array
+ * @dest: destination
+ * Return: dest
+ */
+
+char *_strcpy(chat *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * _strdup - array for string
+ * @str: string
+ * Return: pointer
+ */
+
+char *_strdup(chat *str)
+{
+	char *dst;
+	unsigned int size;
+
+	if (str == 0)
+	{
+		return (NULL);
+	}
+
+	size = _strlen(str) + 1;
+	dst = (char *) malloc(size * sizeof(char));
+
+	if (dst == 0)
+	{
+		return (NULL);
+	}
+
+	_strcpy(dst, str);
+	return (dst);
+}
